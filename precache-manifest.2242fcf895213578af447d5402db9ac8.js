6fcff98f456e977b655069e6f1ec409e8a627f45
Chawit.O <chawit.ocr@gmail.com>
2019-11-30 12:49:03 +0000

    Updates

---
+++ precache-manifest.2242fcf895213578af447d5402db9ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd7df587f33e7fd41bfdd5fa74e0331",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "8d25db8c0918a1dad2dd",
+    "url": "/personal-website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "45abcfd8e1ddb05e1dd4",
+    "url": "/personal-website/static/js/2.883591b7.chunk.js"
+  },
+  {
+    "revision": "8d25db8c0918a1dad2dd",
+    "url": "/personal-website/static/js/main.f7b797c9.chunk.js"
+  },
+  {
+    "revision": "94c7a169191770039168",
+    "url": "/personal-website/static/js/runtime-main.d412900a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/personal-website/static/media/logo.25bf045c.svg"
+  }
+]);
